9ded6579323fb994c9e77e8064612b52c1a1db5f
anpo879a--tu-dresden.de <andrei.politov@tu-dresden.de>
2023-04-27 19:40:10 +0200

    bug with no training fixing

--- src/features/summarization_train.py
+++ src/features/summarization_train.py
@@ -403,14 +403,16 @@ def main():
         optim="adamw_ort_fused",  # Fused Adam optimizer implemented by ORT
     )
 
-    # config = AutoConfig.from_pretrained(
-    #     model_args.config_name if model_args.config_name else model_args.model_name_or_path,
-    #     cache_dir=model_args.cache_dir,
-    #     revision=model_args.model_revision,
-    #     use_auth_token=True if model_args.use_auth_token else None,
-    # )
+    config = AutoConfig.from_pretrained(
+        #model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        revision=model_args.model_revision,
+        use_auth_token=True if model_args.use_auth_token else None,
+    )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        #model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        model_args.tokenizer_name,
         cache_dir=model_args.cache_dir,
         use_fast=model_args.use_fast_tokenizer,
         revision=model_args.model_revision,
@@ -419,7 +421,7 @@ def main():
     model = PegasusForConditionalGeneration.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
-        #config=config,
+        config=config,
         cache_dir=model_args.cache_dir,
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
